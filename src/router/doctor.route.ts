06a90f9807176c39654ff8c53d3a31a4bf15b50c
iamnachiketh <nachikethkc@gmail.com>
2025-02-14 15:04:49 +0530

    [Nachiketha]Added: Implemented cancelling apoinement, list of doctors, changing availability, complete appointment, doctor dashboard, doctor profile, update doctor profile

--- src/router/doctor.route.ts
+++ src/router/doctor.route.ts
@@ -6,6 +6,13 @@ const doctorRouter = express.Router();
 
 doctorRouter.post("/login", DoctorController.handleLoginDoctor);
 doctorRouter.get("/appointments", authDoctor, DoctorController.handelAppointmentsDoctor);
+doctorRouter.put("/cancel-appointment", authDoctor, DoctorController.handelAppointmentCancel);
+doctorRouter.get("/list", DoctorController.handelDoctorList);
+doctorRouter.post("/change-availability", authDoctor, DoctorController.handelChangeAvailablity);
+doctorRouter.put("/complete-appointment", authDoctor, DoctorController.handelAppointmentComplete);
+doctorRouter.get("/dashboard", authDoctor, DoctorController.handelDoctorDashboard);
+doctorRouter.get("/profile", authDoctor, DoctorController.handelDoctorProfile);
+doctorRouter.put("/update-profile", authDoctor, DoctorController.handelUpdateDoctorProfile);
 
 
 export default doctorRouter;
--- src/service/doctor.service.ts
+++ src/service/doctor.service.ts
@@ -42,3 +42,143 @@ export const appointmentDoctor = async function (docId: string) {
         return { status: httpCode.INTERNAL_SERVER_ERROR, message: error.message, data: null };
     }
 }
+
+export const cancelAppointmentDoctor = async function (appointmentId: string, docId: string) {
+
+    try {
+        const appointmentData = await appointmentModel.findById(appointmentId)
+        if (appointmentData && appointmentData.docId === docId) {
+            await appointmentModel.findByIdAndUpdate(appointmentId, { cancelled: true })
+            return { status: httpCode.OK, message: 'Appointment Cancelled', data: null };
+        }
+        return { status: httpCode.BAD_REQUEST, message: "Appointment Cancelled", data: null };
+    } catch (error: any) {
+        return { status: httpCode.INTERNAL_SERVER_ERROR, message: error.message, data: null }
+    }
+
+}
+
+
+export const doctorList = async function () {
+    try {
+        const doctors = await doctorModel.find({}, { __v: 0 });
+
+        if (!doctors) {
+            return { status: httpCode.NOT_FOUND, message: "Doctors not found", data: null };
+        }
+
+        return { status: httpCode.OK, message: "List os Doctors", data: doctors };
+
+    } catch (error: any) {
+        return { status: httpCode.INTERNAL_SERVER_ERROR, message: error.message, data: null };
+    }
+}
+
+
+export const changeAvailablity = async function (docId: string) {
+    try {
+        const docData = await doctorModel.findById(docId)
+        await doctorModel.findByIdAndUpdate(docId, { available: !docData?.available });
+        return { status: httpCode.OK, message: "Availablity Changed", data: null };
+    } catch (error: any) {
+        return { status: httpCode.INTERNAL_SERVER_ERROR, message: error.message, data: null };
+    }
+}
+
+
+export const appointmentComplete = async function (appointmentId: string, docId: string) {
+    try {
+        const appointmentData = await appointmentModel.findById(appointmentId)
+        if (appointmentData && appointmentData.docId === docId) {
+            await appointmentModel.findByIdAndUpdate({ _id: appointmentId, cancelled: false }, { isCompleted: true })
+            return { status: httpCode.OK, message: "Appointment Completed", data: appointmentData };
+        }
+
+        return { status: httpCode.NOT_FOUND, message: `Not Appointment with id ${appointmentId} has been found`, data: null };
+
+    } catch (error: any) {
+        return { status: httpCode.INTERNAL_SERVER_ERROR, message: error.message, data: null };
+    }
+}
+
+
+export const doctorDashboard = async function (docId: string) {
+    try {
+        const appointments = await appointmentModel.find({ _id: docId, cancelled: false }, { __v: 0 });
+
+        if (!appointments) {
+            return { status: httpCode.NOT_FOUND, message: `No Appointments found for doctor with id ${docId}`, data: null };
+        }
+
+        let earnings = 0
+
+        appointments.map((item) => {
+            if (item.isCompleted || item.payment) {
+                earnings += item.amount
+            }
+        })
+
+        let patients: any[] = []
+
+        appointments.map((item) => {
+            if (!patients.includes(item.patientId)) {
+                patients.push(item.patientId)
+            }
+        })
+
+
+        const dashData = {
+            earnings,
+            appointments: appointments.length,
+            patients: patients.length,
+            latestAppointments: appointments.reverse()
+        }
+
+        return { status: httpCode.OK, message: "Dashboard", data: dashData };
+
+    } catch (error: any) {
+        return { status: httpCode.INTERNAL_SERVER_ERROR, message: error.message, data: null };
+    }
+}
+
+
+export const doctorProfile = async function (docId: string) {
+    try {
+        const profileData = await doctorModel.findById(docId);
+
+        if (!profileData) {
+            return { status: httpCode.NOT_FOUND, message: "Doctor not found", data: null };
+        }
+
+        return { status: httpCode.OK, message: "Doctor Details", data: profileData };
+
+    } catch (error: any) {
+        return { status: httpCode.INTERNAL_SERVER_ERROR, message: error.message, data: null };
+    }
+}
+
+
+
+export const updateProfile = async function (data: {
+    docId: string,
+    fees: number,
+    address: string,
+    available: string
+}) {
+    try {
+        const response = await doctorModel.findByIdAndUpdate(data.docId, {
+            fees: data.fees,
+            address: data.address,
+            available: data.available
+        }, { new: true });
+
+        if (!response) {
+            return { status: httpCode.NOT_FOUND, message: `Doctor with id ${data.docId} not present`, data: null }
+        }
+
+        return { status: httpCode.OK, message: "Doctor Details", data: response };
+
+    } catch (error: any) {
+        return { status: httpCode.INTERNAL_SERVER_ERROR, message: error.message, data: null };
+    }
+}
